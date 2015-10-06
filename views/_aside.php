5676e13ef8f18a92981e3276f698501ca515622f
andyhatman <liaros88@gmail.com>
2015-10-06 18:32:57 +0300

    massiv

---
+++ views/_aside.php
@@ -0,0 +1,11 @@
+<aside class="column column3">
+				<h2>Каталог</h2>
+				<ul>
+					<?php $catMenu = ['Маленькие уточки', 'Утки с моторчиком', 'Подводные уточки', 'Уточки ручной работы', 'Говорящие уточки'] ?>
+					<li><a href="catalog.php"><?php echo $catMenu[0];?></a></li>
+					<li><a href="catalog.php"><?php echo $catMenu[1];?></a></li>
+					<li><a href="catalog.php"><?php echo $catMenu[2];?></a></li>
+					<li><a href="catalog.php"><?php echo $catMenu[3];?></a></li>
+					<li><a href="catalog.php"><?php echo $catMenu[4];?></a></li>
+				</ul>
+</aside>
