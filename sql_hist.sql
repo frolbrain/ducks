9bd9a71605a8cd9efbd9c832dfc772424545fa7f
andyhatman <liaros88@gmail.com>
2015-11-03 17:43:30 +0300

    autocommit

--- sql_hist.sql
+++ sql_hist.sql
@@ -1,28 +1,18 @@
-<ENTRY timestamp='21:28:51'>CREATE TABLE `img` (&#x0A;    `id` int(100) NOT NULL AUTO_INCREMENT,&#x0A;    `file_name` varchar(32) NOT NULL,&#x0A;    `url` varchar(32) NOT NULL,&#x0A;    PRIMARY KEY (`id`)&#x0A;)  ENGINE=INNODB DEFAULT CHARSET=utf8</ENTRY>
-<ENTRY timestamp='21:32:47'>CREATE TABLE `categories` (&#x0A;    `id` int(100) NOT NULL AUTO_INCREMENT,&#x0A;    `title` varchar(255) NOT NULL,&#x0A;    `description` text NOT NULL,&#x0A;&#x09;`img_url` varchar(64) NOT NULL,&#x0A;    PRIMARY KEY (`id`)&#x0A;)  ENGINE=INNODB DEFAULT CHARSET=utf8</ENTRY>
-<ENTRY timestamp='~'>CREATE TABLE `categories` (&#x0A;  `id` int(100) NOT NULL AUTO_INCREMENT,&#x0A;  `title` varchar(255) NOT NULL,&#x0A;  `description` text NOT NULL,&#x0A;  `img_url` varchar(64) NOT NULL,&#x0A;  PRIMARY KEY (`id`)&#x0A;) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8</ENTRY>
-<ENTRY timestamp='22:14:27'>ALTER TABLE `duck_store`.`categories` &#x0A;DROP PRIMARY KEY,&#x0A;ADD PRIMARY KEY (`id`, `img_url`)</ENTRY>
-<ENTRY timestamp='22:16:19'>ALTER TABLE `duck_store`.`img` &#x0A;DROP PRIMARY KEY,&#x0A;ADD PRIMARY KEY (`id`, `url`)</ENTRY>
-<ENTRY timestamp='22:17:04'>ALTER TABLE `duck_store`.`products` &#x0A;DROP PRIMARY KEY,&#x0A;ADD PRIMARY KEY (`id`, `category_id`)</ENTRY>
-<ENTRY timestamp='22:31:59'>ALTER TABLE `duck_store`.`products` &#x0A;CHANGE COLUMN `id` `id` INT(100) NOT NULL</ENTRY>
-<ENTRY timestamp='22:46:34'>ALTER TABLE `duck_store`.`products` &#x0A;CHANGE COLUMN `category_id` `category_id` INT(100) NOT NULL DEFAULT &apos;0&apos; ,&#x0A;DROP PRIMARY KEY,&#x0A;ADD PRIMARY KEY (`id`)</ENTRY>
-<ENTRY timestamp='22:48:55'>ALTER TABLE products&#x0A;ADD FOREIGN KEY (category_id)&#x0A;REFERENCES categories(id)</ENTRY>
-<ENTRY timestamp='22:54:02'>ALTER TABLE `duck_store`.`products` &#x0A;DROP FOREIGN KEY `products_ibfk_1`</ENTRY>
-<ENTRY timestamp='~'>ALTER TABLE `duck_store`.`products` &#x0A;ADD CONSTRAINT `products_ibfk_1`&#x0A;  FOREIGN KEY (`category_id`)&#x0A;  REFERENCES `duck_store`.`categories` (`id`)&#x0A;  ON DELETE RESTRICT&#x0A;  ON UPDATE CASCADE</ENTRY>
-<ENTRY timestamp='22:56:27'>ALTER TABLE categories&#x0A;ADD FOREIGN KEY (img_url)&#x0A;REFERENCES img(url)</ENTRY>
-<ENTRY timestamp='22:57:32'>ALTER TABLE `duck_store`.`categories` &#x0A;DROP PRIMARY KEY,&#x0A;ADD PRIMARY KEY (`id`)</ENTRY>
-<ENTRY timestamp='22:57:43'>ALTER TABLE `duck_store`.`img` &#x0A;DROP PRIMARY KEY,&#x0A;ADD PRIMARY KEY (`id`)</ENTRY>
-<ENTRY timestamp='22:57:55'>ALTER TABLE `duck_store`.`img` &#x0A;CHANGE COLUMN `url` `url` VARCHAR(64) NOT NULL</ENTRY>
-<ENTRY timestamp='22:58:01'>ALTER TABLE categories&#x0A;ADD FOREIGN KEY (img_url)&#x0A;REFERENCES img(url)</ENTRY>
-<ENTRY timestamp='23:02:04'>ALTER TABLE categories&#x0A;ADD FOREIGN KEY (img_id)&#x0A;REFERENCES img(id)</ENTRY>
-<ENTRY timestamp='23:02:16'>ALTER TABLE `duck_store`.`categories` &#x0A;CHANGE COLUMN `img_url` `img_id` INT(100) NOT NULL</ENTRY>
-<ENTRY timestamp='23:02:56'>ALTER TABLE `duck_store`.`categories` &#x0A;CHANGE COLUMN `img_url` `img_id` INT(100) NOT NULL</ENTRY>
-<ENTRY timestamp='23:03:06'>ALTER TABLE categories&#x0A;ADD FOREIGN KEY (img_id)&#x0A;REFERENCES img(id)</ENTRY>
-<ENTRY timestamp='23:07:22'>ALTER TABLE `duck_store`.`products` &#x0A;CHANGE COLUMN `price` `price` DECIMAL(10,2) NOT NULL AFTER `description`,&#x0A;ADD COLUMN `img_id` INT(100) NOT NULL AFTER `category_id`</ENTRY>
-<ENTRY timestamp='23:08:33'>ALTER TABLE products&#x0A;ADD FOREIGN KEY (img_id)&#x0A;REFERENCES img(id)</ENTRY>
-<ENTRY timestamp='23:09:19'>ALTER TABLE `duck_store`.`categories` &#x0A;DROP FOREIGN KEY `categories_ibfk_1`</ENTRY>
-<ENTRY timestamp='23:09:28'>ALTER TABLE products&#x0A;ADD FOREIGN KEY (img_id)&#x0A;REFERENCES img(id)</ENTRY>
-<ENTRY timestamp='23:10:01'>ALTER TABLE `duck_store`.`categories` &#x0A;DROP COLUMN `img_id`,&#x0A;DROP INDEX `img_id`</ENTRY>
-<ENTRY timestamp='23:12:21'>ALTER TABLE products&#x0A;ADD FOREIGN KEY (img_id)&#x0A;REFERENCES img(id)</ENTRY>
-<ENTRY timestamp='23:12:46'>ALTER TABLE `duck_store`.`products` &#x0A;DROP FOREIGN KEY `products_ibfk_2`</ENTRY>
-<ENTRY timestamp='~'>ALTER TABLE `duck_store`.`products` &#x0A;ADD CONSTRAINT `products_ibfk_2`&#x0A;  FOREIGN KEY (`img_id`)&#x0A;  REFERENCES `duck_store`.`img` (`id`)&#x0A;  ON UPDATE CASCADE</ENTRY>
+ALTER TABLE `duck_store`.`products` &#x0A;CHANGE COLUMN `id` `id` INT(100) NOT NULL</ENTRY>
+ALTER TABLE `duck_store`.`products` &#x0A;CHANGE COLUMN `category_id` `category_id` INT(100) NOT NULL DEFAULT &apos;0&apos; ,&#x0A;DROP PRIMARY KEY,&#x0A;ADD PRIMARY KEY (`id`)</ENTRY>
+ALTER TABLE products&#x0A;ADD FOREIGN KEY (category_id)&#x0A;REFERENCES categories(id)</ENTRY>
+ALTER TABLE `duck_store`.`products` &#x0A;ADD CONSTRAINT `products_ibfk_1`&#x0A;  FOREIGN KEY (`category_id`)&#x0A;  REFERENCES `duck_store`.`categories` (`id`)&#x0A;  ON DELETE RESTRICT&#x0A;  ON UPDATE CASCADE</ENTRY>
+ALTER TABLE categories&#x0A;ADD FOREIGN KEY (img_url)&#x0A;REFERENCES img(url)</ENTRY>
+ALTER TABLE `duck_store`.`categories` &#x0A;DROP PRIMARY KEY,&#x0A;ADD PRIMARY KEY (`id`)</ENTRY>
+ALTER TABLE `duck_store`.`img` &#x0A;DROP PRIMARY KEY,&#x0A;ADD PRIMARY KEY (`id`)</ENTRY>
+ALTER TABLE `duck_store`.`img` &#x0A;CHANGE COLUMN `url` `url` VARCHAR(64) NOT NULL</ENTRY>
+ALTER TABLE categories&#x0A;ADD FOREIGN KEY (img_url)&#x0A;REFERENCES img(url)</ENTRY>
+ALTER TABLE categories&#x0A;ADD FOREIGN KEY (img_id)&#x0A;REFERENCES img(id)</ENTRY>
+ALTER TABLE categories&#x0A;ADD FOREIGN KEY (img_id)&#x0A;REFERENCES img(id)</ENTRY>
+ALTER TABLE `duck_store`.`products` &#x0A;CHANGE COLUMN `price` `price` DECIMAL(10,2) NOT NULL AFTER `description`,&#x0A;ADD COLUMN `img_id` INT(100) NOT NULL AFTER `category_id`</ENTRY>
+ALTER TABLE products&#x0A;ADD FOREIGN KEY (img_id)&#x0A;REFERENCES img(id)</ENTRY>
+ALTER TABLE `duck_store`.`categories` &#x0A;DROP FOREIGN KEY `categories_ibfk_1`</ENTRY>
+ALTER TABLE products&#x0A;ADD FOREIGN KEY (img_id)&#x0A;REFERENCES img(id)</ENTRY>
+ALTER TABLE `duck_store`.`categories` &#x0A;DROP COLUMN `img_id`,&#x0A;DROP INDEX `img_id`</ENTRY>
+ALTER TABLE products&#x0A;ADD FOREIGN KEY (img_id)&#x0A;REFERENCES img(id)</ENTRY>
+ALTER TABLE `duck_store`.`products` &#x0A;ADD CONSTRAINT `products_ibfk_2`&#x0A;  FOREIGN KEY (`img_id`)&#x0A;  REFERENCES `duck_store`.`img` (`id`)&#x0A;  ON UPDATE CASCADE</ENTRY>
--- sql_hist13.sql
+++ sql_hist13.sql
@@ -7,18 +7,6 @@ CREATE TABLE `products` (
   `price` decimal(10,2) NOT NULL;
   PRIMARY KEY (`id`);
 ) ENGINE=INNODB DEFAULT CHARSET UTF8;
-CREATE TABLE `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=INNODB DEFAULT CHARSET UTF8;
-CREATE TABLE `photos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `duck_id` int(11) NOT NULL,
-  `photo` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=INNODB DEFAULT CHARSET UTF8;
 CREATE TABLE `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
@@ -27,7 +15,3 @@ CREATE TABLE `products` (
   `price` decimal(10 , 2 ) NOT NULL,
   PRIMARY KEY (`id`)
 )  ENGINE=INNODB DEFAULT CHARSET UTF8;
-
-CREATE TABLE `products` (&#x0A;    `id` int(11) NOT NULL AUTO_INCREMENT,&#x0A;    `title` varchar(255) NOT NULL,&#x0A;    `description` text NOT NULL,&#x0A;    `category_id` int(11) DEFAULT NULL,&#x0A;    `price` decimal(10 , 2 ) NOT NULL,&#x0A;    PRIMARY KEY (`id`)&#x0A;)  ENGINE=INNODB DEFAULT CHARSET=utf8</ENTRY>
-CREATE TABLE `categories` (&#x0A;    `id` int(11) NOT NULL AUTO_INCREMENT,&#x0A;    `title` varchar(255) NOT NULL,&#x0A;    `description` text NOT NULL,&#x0A;    PRIMARY KEY (`id`)&#x0A;)  ENGINE=INNODB DEFAULT CHARSET=utf8</ENTRY>
-CREATE TABLE `photos` (&#x0A;    `id` int(11) NOT NULL AUTO_INCREMENT,&#x0A;    `duck_id` int(11) NOT NULL,&#x0A;    `photo` varchar(255) NOT NULL,&#x0A;    PRIMARY KEY (`id`)&#x0A;)  ENGINE=INNODB DEFAULT CHARSET=utf8</ENTRY>
