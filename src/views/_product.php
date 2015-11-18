a5c68d950e4469dd96c3207cf584c668f1a33e81
andyhatman <liaros88@gmail.com>
2015-11-18 15:57:58 +0300

    autocommit

--- src/views/_product.php
+++
@@ -1,6 +0,0 @@
-<div class="item-block column column4">
-  <a href='index.php?page=product&id=<?= $product['id']; ?>' class="item">
-    <img src="img/item.jpeg" alt="уточка">
-  </a>
-  <a href="index.php?page=add_to_card" class="btn-basket">В Корзину</a>
-</div>
--- src/views/head.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  	<meta charset="UTF-8">
-  	<title>Duck Special</title>
-  	<link href="css/normalize.css" rel="stylesheet">
-  	<link href="css/style.css" rel="stylesheet">
-</head>
